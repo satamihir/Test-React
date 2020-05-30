e84c899ab520cfdcf738bd156573e10bf96e80ae
satamihir <satamihir@gmail.com>
2020-05-30 12:44:14 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+import person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      {id: '56', name: 'Max', age: 28 },
+      {id: '65', name: 'Manu', age: 29 },
+      {id: '18', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+
+  nameChangedHandler = ( event ) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  }
+
+  deletePersonHandler = (personIndex) => {
+      //const persons = this.state.persons.slice();
+
+      const persons = [...this.state.persons];
+      persons.splice(personIndex,1);
+      this.setState({persons:persons});
+    }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+              {this.state.persons.map((person, index) => {
+                return <Person 
+                    name = {person.name}
+                    age = {person.age}
+                    key = {person.id}
+                    click = {() => this.deletePersonHandler(index)} />  
+              })}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default App;
